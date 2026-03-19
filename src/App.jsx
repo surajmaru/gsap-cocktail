c4b7f109011de4b4aa2d8241f17b0bf054051fca
Suraj <suraj123maru@gmail.com>
2026-03-19 14:00:01 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { ScrollSmoother, ScrollTrigger, SplitText } from 'gsap/all'
+import gsap from 'gsap/gsap-core.js'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl font-bold underline text-indigo-300'>
+        Hello GSAP!
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
