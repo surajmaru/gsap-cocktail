68cbf260dca72f8d02889478652bec42d6b62895
Suraj <suraj123maru@gmail.com>
2026-03-20 14:01:32 +0530

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,34 +61,29 @@ function About() {
         <div className='top-grid'>
 
             <div className='md:col-span-3'>
-                <div className='noisy'>
+                <div className='noisy' />
                     <img src='/images/abt1.png' alt='grid-img-1' />
-                </div>
             </div>
 
             <div className='md:col-span-6'>
-                <div className='noisy'>
+                <div className='noisy' />
                     <img src='/images/abt2.png' alt='grid-img-1' />
-                </div>
             </div>
 
             <div className='md:col-span-3'>
-                <div className='noisy'>
+                <div className='noisy' />
                     <img src='/images/abt5.png' alt='grid-img-1' />
-                </div>
             </div>
         </div>
 
         <div className='bottom-grid'>
             <div className='md:col-span-8'>
-                <div className='noisy'>
+                <div className='noisy' />
                     <img src='/images/abt3.png' alt='grid-img-1' />
-                </div>
             </div>
             <div className='md:col-span-4'>
-                <div className='noisy'>
+                <div className='noisy' />
                     <img src='/images/abt4.png' alt='grid-img-1' />
-                </div>
             </div>
         </div>
 
